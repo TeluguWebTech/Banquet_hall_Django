8e7794d5825f6c6ca4f91f541c4fcbe7cc254696
TeluguWebTech <kalyan.vza@gmail.com>
2025-01-21 19:18:03 +0530

    vercel configured

---
+++ landing/build.files.sh
@@ -0,0 +1,3 @@
+
+pip install -r requirements.txt
+python3.13.1 manage.py collectstatic
--- landing/landing/settings.py
+++ landing/landing/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(ty_ytv+oyg4m7@9^g-s_dsf(nzmwdv^htg2yw4-voqk0cmo47'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', 'now.sh', '127.0.0.1', 'localhost']
 
 
 # Application definition
