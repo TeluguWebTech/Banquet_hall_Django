8fae0a61f777ba8bc389688e71bc5f67f1d4c796
TeluguWebTech <kalyan.vza@gmail.com>
2025-01-21 18:34:54 +0530

    status os updated

--- landing/landing/settings.py
+++ landing/landing/settings.py
@@ -125,9 +125,9 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'landing/static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'landing/static')]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
